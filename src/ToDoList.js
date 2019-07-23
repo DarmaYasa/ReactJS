2e7a2b7b792fb6bafb6e3e51b7f3fb3d415f6ffb
DarmaYasa <yasa13darma@gmail.com>
2019-07-23 15:29:12 +0900

    learn how to use state

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -3,13 +3,36 @@ import TodoListItem from './TodoListItem'
 import './Style.css'
 
 export default class ToDoList extends React.Component {
+    state = {
+        list_title: "I'm the title",
+        todos: [
+            {
+                title: 'Go jogging',
+                completed: false
+            },
+            {
+                title: 'Run on the beach',
+                completed: true
+            }
+        ]
+    }
+
+
     render() {
         return (
             <div className="todo-list">
                 <h2>to do list...</h2>   
                 <label>Title</label> <br />
-                <input placeholder="Enter title..." type="text" />
+                <input placeholder="Enter title..." type="text" value={this.state.list_title}/>
                 <input placeholder="Todo Item Name..." type="text" />
+                <br />
+                {
+                    this.state.todos.map(todo => {
+                        return (
+                            <TodoListItem />
+                        )
+                    })
+                }
                 <button>Add Item to List</button>
             </div>
         )
