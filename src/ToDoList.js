b1afcdc7993ca9e7c4e806c0b523eb3bf561794b
DarmaYasa <yasa13darma@gmail.com>
2019-07-23 15:05:18 +0900

    Styling CSS

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import TodoListItem from './TodoListItem'
+import './Style.css'
 
 export default class ToDoList extends React.Component {
     render() {
         return (
-            <div>
+            <div className="todo-list">
                 to do list...   
                 <TodoListItem />
                 <TodoListItem />
